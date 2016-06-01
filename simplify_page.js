95cbd74fd446903f4a4a3ec61c6b78db1f4ba062
Guillaume Jimenez <guillaume.jim@gmail.com>
2016-06-01 16:46:11 -0700

    Create simplify_page.js

---
+++ simplify_page.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        http://www.streaming-hub.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    //alert("coucou");
+    document.getElementById("header-container").style.display = "none";
+   
+})();
